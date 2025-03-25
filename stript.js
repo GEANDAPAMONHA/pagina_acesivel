1cde73352197e07ee386bdb8ddb64f197fa54492
GEANDAPAMONHA <144158712+GEANDAPAMONHA@users.noreply.github.com>
2025-03-25 11:19:16 -0300

    dia 25

---
+++ stript.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function (){
+const aumentarfontebotao = document.getElementById ('aumenta-botao')
+const diminuebotao = document.getElementById ('diminue-botao')
+
+let tamanhoAtualfonte = 1;
+
+aumentarfontebotao.addEventListener ('click')
+
+
+})
